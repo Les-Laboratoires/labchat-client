f9704e6d05c6347f85e7042ad77de236a7a3b93d
23felons23 <slavikzhyhota@gmail.com>
2021-02-22 22:27:58 +0100

    feat(channels): create basic channelList and text channel style

--- src/components/ChannelList.svelte
+++ src/components/ChannelList.svelte
@@ -2,6 +2,12 @@
     import Channel from "./Channel.svelte";
 </script>
 
-<div class="h-full w-64 bg-gray-700">
-    <Channel />
+<div class="w-64 h-full    bg-gray-700">
+    <div class="w-full h-14     border-b border-gray-800">
+    </div>
+
+    <!--Channels container-->
+    <nav class="px-3 py-2">
+        <Channel type="text" name="hello-noob"/>
+    </nav>
 </div>
