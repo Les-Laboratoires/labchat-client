265614fd6f6e8f1a9bef4dd7d8198069e3ffdd93
23felons23 <slavikzhyhota@gmail.com>
2021-02-22 22:29:00 +0100

    refactor: update chatInput style

--- src/components/Chat/ChatInput.svelte
+++ src/components/Chat/ChatInput.svelte
@@ -3,16 +3,21 @@ Contient un input text.
 Contient un emoji/gif picker et un embed maker.
 -->
 <script lang="ts">
-    let message: string = "";
+  let message: string = ""
 </script>
 
 <div class="px-6 w-full">
-    <div class="w-full    bg-gray-500    rounded-lg border border-2 border-gray-50    flex items-center">
-
-        <div class="mx-3"></div>
-        <input aria-label="Send a message" class="py-2    rounded-lg    bg-gray-500    text-gray-50    flex-grow  focus:outline-none"
-               bind:value={message} type="text">
-        <div class="flex"></div>
-
-    </div>
+  <div
+    class="w-full    bg-gray-500    rounded-lg     shadow-lg   flex items-center">
+    <div class="mx-3"> </div>
+    <input
+      aria-label="Send a message"
+      aria-multiline="true"
+      spellcheck="true"
+      class="py-3     rounded-lg    bg-gray-500    text-gray-50    flex-grow  focus:outline-none  break-words whitespace-pre-wrap leading-relaxed align-middle"
+      bind:value={message}
+      type="text"
+      style="line-height: 0.5rem"/>
+    <div class="flex"> </div>
+  </div>
 </div>
